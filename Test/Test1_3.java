397cdf8296df01acc32953f1e7c54b5f34f404df
farrah25 <61194819+farrah25@users.noreply.github.com>
2020-03-12 15:48:57 +0800

    Add files via upload

---
+++ Test/Test1_3.java
@@ -0,0 +1,11 @@
+class Test1_3 {
+
+public static void main(String args[]){
+      String s1 = "Hello You!";
+      String s2 = "Hello You";
+      String s3 = "helloyou!";
+      
+      System.out.println(s1.equals(s2));
+      System.out.println(s1.equals(s3));
+   }
+}
---
+++ Test/Test_Q1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Test_Q1
+{
+  public static void main (String []args){
+  Scanner scan = new Scanner(System.in);
+   
+  String str; 
+   System.out.print("Enter any message:");
+   str=scan.nextLine();
+   
+   System.out.print(str);
+   
+     }
+   }
+
+  
